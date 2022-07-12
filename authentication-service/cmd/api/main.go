a855029ec23b860d73fdbe4f86856132b6aee49f
tirzasrwn <tirzasrwn@gmail.com>
2022-07-12 21:49:02 +0800

    feat: Setting up authentication service.

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8000"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Printf("Starting authentication service %s ...\n", webPort)
+
+	// TODO connect to database
+
+	// set up config
+	app := Config{}
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
