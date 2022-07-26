0f36b428e0b4a42fdfbed135d1d651c8dc8e0381
tirzasrwn <tirzasrwn@gmail.com>
2022-07-26 11:35:00 +0800

    feat: Updating main.go to start the listener function.

--- listener-service/main.go
+++ listener-service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"listener/event"
 	"log"
 	"math"
 	"os"
@@ -18,13 +19,20 @@ func main() {
 		os.Exit(1)
 	}
 	defer rabbitConn.Close()
-	log.Println("Connected to RabbitMQ!")
-	// Start listening for messages.
 
+	// Start listening for messages.
+	log.Println("Listening for and consuming RabbitMQ messages ...")
 	// Create consumer.
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
 
 	// Watch the queue and consume events.
-
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func connect() (*amqp.Connection, error) {
@@ -38,6 +46,7 @@ func connect() (*amqp.Connection, error) {
 			fmt.Println("RabbitMQ not yet ready ...")
 			counts++
 		} else {
+			log.Println("Connected to RabbitMQ!")
 			connection = c
 			break
 		}
